9f1e9410a6f6703223f352c0089330489b3b6768
Maria Francisca Brauning <franbrauning@gmail.com>
2021-11-10 12:26:20 -0500

    backend created

---
+++ server.js
@@ -0,0 +1,102 @@
+////////////////////////////
+// DEPENDENCIES
+////////////////////////////
+require("dotenv").config();
+const { PORT = 3000, DATABASE_URL } = process.env;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+
+
+////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////
+mongoose.connect(DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+mongoose.connection
+    .on("open", () => console.log("connected to mongoose"))
+    .on("close", () => console.log("disconnected from mongoose"))
+    .on("error", (error) => console.log(error))
+
+
+////////////////////////////
+// MODELS
+////////////////////////////
+const CheeseSchema = new mongoose.Schema({
+    name: String,
+    countryOfOrigin: String,
+    image: String,
+})
+
+const Cheese = mongoose.model("Cheese", CheeseSchema);
+
+
+////////////////////////////
+// MIDDLEWARE
+////////////////////////////
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+
+////////////////////////////
+// ROUTES
+////////////////////////////
+
+// test route
+app.get("/", (req, res) => {
+    res.send("app is working")
+});
+
+// index route
+app.get("/cheese", async (req, res) => {
+    try {
+        res.json(await Cheese.find({}))
+    } catch(error) {
+        res.status(400).json(error)
+    }
+})
+
+// create route
+app.post("/cheese", async (req, res) => {
+    try {
+        res.json(await Cheese.create(req.body))
+    } catch(error) {
+        res.status(400).json(error)
+    }
+})
+
+// update route
+app.put("/cheese/:id", async (req, res) => {
+    try {
+        res.json(
+            await Cheese.findByIdAndUpdate(req.params.id,req.body, { new: true })
+        )
+    } catch(error) {
+        res.status(400).json(error)
+    }
+})
+
+// destroy route
+app.delete("/cheese/:id", async (req, res) =>{
+    try {
+        res.json (await Cheese.findByIdAndRemove(req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+} )
+
+
+
+
+
+
+////////////////////////////
+// LISTENER
+////////////////////////////
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
